7f17639af66b3c8ff5549024bd7c0b8cf9ad3d40
Andrew B <37745667+AndrianBalanescu@users.noreply.github.com>
2022-01-19 15:37:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+
+Before starting, make sure you are logged in to CodePen, at the end of the test you will need to export your solution as a zip. The export function can be found in the bottom right corner.
+
+The task is to implement the component seen in the image using only HTML and CSS.
+You will be given 40 minutes to achieve as close to pixel perfection as possible while also ensuring good code quality.
+
+The test is extremely stressful. It will show how comfortable you are with basic technologies. In order to get a good result in the time given you will need to work in an organized manner to avoid having to redo things over and over.
+
+Implement the component as if you were actually going to use it, e.g. make the text field editable. No javascript is required.
+
+The CSS editor is set to use Less as the preprocessor. You may change it to use SCSS should you prefer.
+
+Everything needed to complete the task is available here. Don't remove any existing code, simply add your own. You are allowed to look up external references.
+
+Your component will render as you type and be positioned beneath the image. If it is no longer updating you likely have a syntax error.
+
+
+We hope you do well and thank you for taking the time!
+
+
+*/
